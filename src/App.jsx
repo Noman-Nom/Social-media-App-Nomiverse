848fdbd2ab29c91f68a100cfb2e321f6475f358b
Noman-Nom <noman5456318@gmail.com>
2023-11-04 19:37:56 +0500

    login auth completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,70 @@
-import React from 'react'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import Profile from './pages/Profile/Profile'
-import Home from './pages/Home/Home'
+import React from "react";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import Home from "./pages/Home/Home";
 import {
   createBrowserRouter,
+  Navigate,
   Outlet,
   RouterProvider,
 } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-import LeftBar from './components/LeftBar/LeftBar';
-import RightBar from './components/RightBar/RightBar';
+import Navbar from "./components/Navbar/Navbar";
+import LeftBar from "./components/LeftBar/LeftBar";
+import RightBar from "./components/RightBar/RightBar";
 
 const App = () => {
+  const currentUser = false;
 
-
-  const Layout = ()=>{
-    return(
+  const Layout = () => {
+    return (
       <div>
-        <Navbar/>
-        <div style={{display:"flex"}}>
-          <LeftBar/>
-          <Outlet/>
-          <RightBar/>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
         </div>
       </div>
-    )
-  }
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout/>,
-      children:[
-        {path:"/",
-        element: <Home/>
-
-
-        },
-        {path:"/profile/:id",
-        element: <Profile/>
-
-
-        },
-      ]
+      element: (
+        <ProtectedRoute>  
+            // mere pas ak error arha tha sirf protectedRoute k p small likhne se   function that returns a component. In React, you need to capitalize component names to distinguish them from regular HTML elements
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/profile/:id", element: <Profile /> },
+      ],
     },
     {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
   ]);
   return (
     <div>
-
-<RouterProvider router={router} />
-
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
