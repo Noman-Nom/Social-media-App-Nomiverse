60abfb70423d6f313845913ce83ef7117a892c01
Noman-Nom <noman5456318@gmail.com>
2023-11-04 20:01:40 +0500

    navbar page working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import './Navbar.scss'
-import HomeIcon from '@mui/icons-material/Home';
-import NightlightIcon from '@mui/icons-material/Nightlight';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import AppsIcon from '@mui/icons-material/Apps';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-
+import React from "react";
+import "./Navbar.scss";
+import HomeIcon from "@mui/icons-material/Home";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import AppsIcon from "@mui/icons-material/Apps";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="left">
+        <Link style={{ textDecoration: "none" }} to="/">
+          <h4>
+            Nomi<span>Verse</span>
+          </h4>
+        </Link>
 
-export default Navbar
+        <HomeIcon />
+        <NightlightIcon />
+        <AppsIcon />
+
+        <div className="search">
+          <SearchIcon/>
+          <input type="text"  placeholder="Search..."/>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Navbar;
