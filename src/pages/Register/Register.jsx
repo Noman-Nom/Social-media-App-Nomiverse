20d79e7fa3f839427beb6d734b58c9cc3c16c6e0
Noman-Nom <noman5456318@gmail.com>
2023-11-04 15:42:05 +0500

    register page working

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,7 +3,28 @@ import './Register.scss'
 
 const Register = () => {
   return (
-    <div className='register'>Register</div>
+    <div className='register'>
+    <div className="card">
+     
+      <div className="right">
+            <h1>Register</h1>
+            <form>
+              <input type="text" placeholder='Enter Name' />
+              <input type="email" placeholder='Enter Email' />
+              <input type="password" placeholder='Enter password' />
+              <button>Register</button>
+            </form>
+      </div>
+      <div className="left">
+        <h1><span>Digital </span>World.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus veniam amet, assumenda obcaecati velit facere dolores ipsam laboriosam ab at! Magnam.</p>
+        <span> you have an account ?  </span>
+        <button>Login</button>
+      </div>
+    </div>
+  
+  
+  </div>
   )
 }
 
