a83593f2c02bca41b794a144642575e695efd494
Noman-Nom <noman5456318@gmail.com>
2023-11-05 02:26:07 +0500

    rightbar working

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -10,6 +10,16 @@ const RightBar = () => {
           <span>Suggestion For You</span>
           <div className="user">
             <div className="userInfo">
+              <img src="/user1.jpg" alt="" />
+              <span>M Noman</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
               <img src="https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
               <span>M Noman</span>
             </div>
@@ -17,6 +27,49 @@ const RightBar = () => {
               <button>Follow</button>
               <button>Dismiss</button>
             </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+            <img src="/user1.jpg" alt="" />
+            <p>
+              <span>M Noman</span> 
+              changed cover picture
+            </p>
+            </div>
+           <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="/user1.jpg" alt="" />
+            <p>
+              <span>M Noman</span> 
+              changed cover picture
+            </p>
+            </div>
+           <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="/user1.jpg" alt="" />
+            <p>
+              <span>M Noman</span> 
+              changed cover picture
+            </p>
+            </div>
+           <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+            <img src="/user1.jpg" alt="" />
+            <p>
+              <span>M Noman</span> 
+              changed cover picture
+            </p>
+            </div>
+           <span>1 min ago</span>
           </div>
         </div>
       </div>
