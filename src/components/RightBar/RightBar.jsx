df7c96a6fae785384d62fb54f0301a1c2a0aa9a3
Noman-Nom <noman5456318@gmail.com>
2023-11-05 01:25:26 +0500

    rightbar working

--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -3,7 +3,25 @@ import './RightBar.scss'
 
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
+    <div className='rightBar'>
+
+      <div className="container">
+        <div className="item">
+          <span>Suggestion For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <span>M Noman</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    </div>
   )
 }
 
