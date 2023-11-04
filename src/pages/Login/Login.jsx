25f9657666494af9406c8b1267194f260221ef66
NomiTechh <muhammadnoman.ned@gmail.com>
2023-11-04 13:22:20 +0500

    setup completed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 import './Login.scss'
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>WelCome To <span>Digital </span>World</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ducimus veniam amet, assumenda obcaecati velit facere dolores ipsam laboriosam ab at! Magnam.</p>
+          <span>Don't you have an account ?  </span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+              <h1>Login</h1>
+              <form>
+                <input type="text" placeholder='Enter Name' />
+                <input type="password" placeholder='Enter password' />
+                <button>Login</button>
+              </form>
+        </div>
+      </div>
+    
+    
+    </div>
   )
 }
 
